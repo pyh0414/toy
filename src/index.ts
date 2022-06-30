4fe89ccbb2545aea7749195f4529b1b7825e84b4
yeonhoPark <whwlsvy12@naver.com>
2022-06-30 20:46:59 +0900

    test

--- src/index.ts
+++ src/index.ts
@@ -24,8 +24,49 @@ const app = express();
     path: "/",
   });
 
-  const port = 3000;
+  const port = 4964;
   app.listen({ port }, () => {
     console.log(`🚀 Server ready at http://localhost:${port}`);
   });
 })();
+
+// import axios, { AxiosResponse } from "axios";
+
+// const sleep = (ms: number) => {
+//   return new Promise((resolve) =>
+//     setTimeout(() => {
+//       console.log(`${ms}초 지남!!`);
+//       resolve(ms);
+//     }, ms)
+//   );
+// };
+
+// const aa = async () => {
+//   return "asd";
+// };
+// const request = async (): Promise<void> => {
+//   console.log("request");
+//   // await axios.get("https://www.naver.com");
+//   await sleep(2);
+//   console.log(`끝 !!`);
+// };
+
+// const test = async () => {
+//   aa().then((c) => console.log(c));
+//   setTimeout(() => {
+//     console.log("@@");
+//   }, 5000);
+//   console.log("@");
+//   await sleep(3);
+//   const length = 10;
+
+//   const promises: Promise<void>[] = [];
+
+//   for (let i = 0; i < length; i++) {
+//     // console.log(i);
+//     // promises.push(request());
+//     // console.log();
+//   }
+// };
+
+// test();
